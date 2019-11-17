00467105537e25a1ef1682fe4cb65e995b14056d
kopi1996 <kopipakee@gmail.com>
2019-11-17 14:20:46 +0530

    Working with hint dialog box listview (finished calender pop up window)

--- app/src/main/java/com/planner/budgetplanner/IncomeAdd.java
+++ app/src/main/java/com/planner/budgetplanner/IncomeAdd.java
@@ -4,16 +4,26 @@ import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Point;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ListView;
 import android.widget.PopupWindow;
 
 public class IncomeAdd extends AppCompatActivity {
+
+    private static final String TAG = "IncomeAdd";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,30 +36,99 @@ public class IncomeAdd extends AppCompatActivity {
                 displayDatePicker();
             }
         });
+        findViewById(R.id.hintBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayHintDialog();
+            }
+        });
+    }
+
+
+    private void displayHintDialog()
+    {
+        int width = 0;
+        int height = 0;
+        Point size = new Point();
+        WindowManager w = getWindowManager();
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)    {
+            w.getDefaultDisplay().getSize(size);
+            width = size.x;
+            height = size.y;
+        }else{
+            Display d = w.getDefaultDisplay();
+            width = d.getWidth();
+            height = d.getHeight();
+        }
+
+
+        int dialogHeight=(int) ((height/100.0)*75);
+
+        final Dialog dialog=new Dialog(this);
+        dialog.setContentView(R.layout.list_view_window);
+
+
+        final WindowManager.LayoutParams lWindowParams = new WindowManager.LayoutParams();
+        lWindowParams.copyFrom(dialog.getWindow().getAttributes());
+        lWindowParams.height = dialogHeight;
+
+        dialog.getWindow().setAttributes(lWindowParams);
+
+
+
+        String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
+        ListView myListView=dialog.findViewById(R.id.listViewDialog);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,R.layout.list_view_text_item,R.id.listItemTxt,countryList){
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position,convertView,parent);
+
+                ViewGroup.LayoutParams params = view.getLayoutParams();
+                params.height = 60;
+                params.width=100;
+
+                view.setLayoutParams(params);
+                return view;
+            }
+        };
+        myListView.setAdapter(adapter);
+
+        dialog.show();
     }
 
     private void displayDatePicker()
     {
-        Display defaultDisplay = getWindowManager().getDefaultDisplay();
-        Point point = new Point();
-        defaultDisplay.getSize(point);
 
-        int width=(int)((point.x/100.0)*90);
-        int height=(int)((point.y/100.0)*70);
-
-
-        Dialog dialog=new Dialog(this);
+        final Dialog dialog=new Dialog(this);
         dialog.setContentView(R.layout.calender_window);
 
-        dialog.show();
 
-//        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//        final View layout = inflater.inflate(R.layout.calender_window, (ViewGroup) findViewById(R.id.calenderLayout));
-//
-//        final PopupWindow pw = new PopupWindow(layout,point.x,point.y,true);
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            pw.setElevation(120f);
-//        }
-//        pw.showAtLocation(findViewById(R.id.incomeLayout), Gravity.CENTER,0,0);
+
+        final DatePicker picker=dialog.findViewById(R.id.datePicker);
+        final View okBtn = dialog.findViewById(R.id.okBtn);
+        final View cancelBtn = dialog.findViewById(R.id.cancelBtn);
+
+
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button pickerBtn = findViewById(R.id.datePickerBtn);
+                pickerBtn.setText(MyUtility.getPickerDate(picker));
+                dialog.dismiss();
+                okBtn.setOnClickListener(null);
+            }
+        });
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                cancelBtn.setOnClickListener(null);
+            }
+        });
+
+        dialog.show();
     }
 }
--- app/src/main/java/com/planner/budgetplanner/MyUtility.java
+++ app/src/main/java/com/planner/budgetplanner/MyUtility.java
@@ -2,6 +2,7 @@ package com.planner.budgetplanner;
 
 import android.text.TextUtils;
 import android.util.Patterns;
+import android.widget.DatePicker;
 
 public class MyUtility {
 
@@ -9,6 +10,10 @@ public class MyUtility {
     public static User currentUser;
     public static boolean isValidEmail(CharSequence target) {
         return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+    }
+    public static String getPickerDate(DatePicker picker)
+    {
+        return picker.getDayOfMonth()+"/"+(picker.getMonth()+1)+"/"+picker.getYear();
     }
 
 }

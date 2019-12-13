c197023dffa6ffef6764b6fff36addfe3db801ca
kopi1996 <kopipakee@gmail.com>
2019-12-14 00:13:25 +0530

    Work with Bar Chart

--- app/src/main/java/com/planner/budgetplanner/ChartActivity.java
+++ app/src/main/java/com/planner/budgetplanner/ChartActivity.java
@@ -4,88 +4,88 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+import com.planner.budgetplanner.Utility.MyUtility;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class ChartActivity extends AppCompatActivity {
 
-    private LineChart lineChart;
+    private BarChart incBarChart,expBarChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
 
-        lineChart=findViewById(R.id.lineChart);
+        incBarChart=findViewById(R.id.incBarChart);
+        expBarChart=findViewById(R.id.expBarChart);
 
-        ArrayList lineEntries = new ArrayList<>();
-        lineEntries.add(new Entry(2f, 0));
-        lineEntries.add(new Entry(4f, 1));
-        lineEntries.add(new Entry(6f, 1));
-        lineEntries.add(new Entry(8f, 3));
-        lineEntries.add(new Entry(7f, 4));
-        lineEntries.add(new Entry(3f, 3));
-        LineDataSet lineDataSet = new LineDataSet(lineEntries, "");
-        LineData lineData = new LineData(lineDataSet);
-        lineChart.setData(lineData);
-        lineDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
-        lineDataSet.setValueTextColor(Color.BLACK);
-        lineDataSet.setValueTextSize(18f);
-        otherChart();
+        ArrayList entries = new ArrayList<>();
+        entries.add(new BarEntry(0, 110.6f));
+        entries.add(new BarEntry(1, 27));
+        entries.add(new BarEntry(2, 38));
+        entries.add(new BarEntry(3, 40));
+        entries.add(new BarEntry(4, 59));
+        entries.add(new BarEntry(5, 64));
+
+        String months[]={"Jan","Feb","Mar","Apr","May","Jun"};
+
+        drawBarChart(incBarChart,"Income",entries,months);
+        drawBarChart(expBarChart,"Expense",entries,months);
     }
 
-    private void otherChart() {
-//        LineChartView lineChartView = findViewById(R.id.chart);
-//        String[] axisData = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept",
-//                "Oct", "Nov", "Dec"};
-//        float[] yAxisData = {50, 17.8f, 15.5f, 30, 20, 60, 15, 40, 45, 10, 90, 18};
-//
-//        List yAxisValues = new ArrayList();
-//        List axisValues = new ArrayList();
-//        Line line = new Line(yAxisValues);
-//
-//        for (int i = 0; i < axisData.length; i++) {
-//            axisValues.add(i, new AxisValue(i).setLabel(axisData[i]));
-//        }
-//
-//        for (int i = 0; i < yAxisData.length; i++) {
-//            PointValue pointValue = new PointValue(i, yAxisData[i]);
-//            pointValue.setLabel(yAxisData[i]+"");
-//            yAxisValues.add(pointValue);
-//        }
-//
-//        List lines = new ArrayList();
-//        lines.add(line);
-//
-//        LineChartData data = new LineChartData();
-//        data.setLines(lines);
-//
-//        lineChartView.setLineChartData(data);
-//        Axis axis = new Axis();
-//        axis.setValues(axisValues);
-//        data.setAxisXBottom(axis);
-//
-//        Axis yAxis = new Axis();
-//        data.setAxisYLeft(yAxis);
-//
-//        line.setColor(Color.parseColor("#9C27B0"));
-//
-//        axis.setTextSize(16);
-//
-//        axis.setTextColor(Color.parseColor("#03A9F4"));
-//
-//        yAxis.setTextColor(Color.parseColor("#03A9F4"));
-//        yAxis.setTextSize(16);
-//
-//        yAxis.setName("Sales in millions");
-//        axis.setName("Month");
+    private void drawBarChart(BarChart barChart,String title,ArrayList<BarEntry> entries,String[] xAxisLabels)
+    {
+        barChart.setDrawBarShadow(false);
+        barChart.setDrawValueAboveBar(true);
+        barChart.setPinchZoom(false);
+        barChart.setDragEnabled(false);
+        barChart.setDoubleTapToZoomEnabled(false);
+        //barChart.getXAxis().setDrawAxisLine(false);
+        barChart.getAxisLeft().setDrawGridLines(false);
+        barChart.getAxisRight().setDrawGridLines(false);
+        barChart.getAxisRight().setEnabled(false);
+        barChart.getXAxis().setDrawGridLines(false);
+        barChart.getDescription().setText("");
 
+        BarDataSet dataSet=new BarDataSet(entries,title);
+        dataSet.setValueTextSize(10);
+        BarData data=new BarData(dataSet);
+        dataSet.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                String txt=Float.toString(value);
+                if (value >= 1000)
+                {
+                    txt= MyUtility.wrapDecPointDouble((value/1000)) +"k";
+                }
+                return txt;
+            }
+        });
+
+        dataSet.setColors(getResources().getColor(R.color.colorAccent));
+        barChart.setData(data);
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(xAxisLabels));
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setGranularity(1);
+        xAxis.setGranularityEnabled(true);
+        xAxis.setAxisLineWidth(1f);
+        barChart.getAxisLeft().setAxisLineWidth(1f);
     }
 }
